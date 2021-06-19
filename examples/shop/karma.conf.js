74f35af249bf0dfd46e9578ed63a98eb3c38e3e8
Luis Vilcarromero <luis.evo_8.10@hotmail.com>
2021-06-19 09:59:33 -0500

    [feat] shop project

--- examples/shop/karma.conf.js
+++ examples/shop/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/workshop-s02'),
+      dir: require('path').join(__dirname, '../../coverage/shop'),
       subdir: '.',
       reporters: [
         { type: 'html' },
